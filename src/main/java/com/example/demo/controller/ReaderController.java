14b98bbb88befe208adb16482a88cea870040de3
Oksana Lashchenko <oksana.lashchenko@globallogic.com>
2021-07-30 17:23:49 +0300

    ReaderController changed

--- src/main/java/com/example/demo/controller/ReaderController.java
+++ src/main/java/com/example/demo/controller/ReaderController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.entity.Book;
@@ -32,8 +33,8 @@ public class ReaderController {
     }
 
     @PostMapping("/take")
-    public ResponseEntity<Book> takeBook(@RequestBody Long readerId,
-                                         @RequestBody Long bookId) {
+    public ResponseEntity<Book> takeBook(@RequestParam("readerId") Long readerId,
+                                         @RequestParam("bookId") Long bookId) {
         return ResponseEntity.ok(readerService.takeBook(readerId, bookId));
     }
 
